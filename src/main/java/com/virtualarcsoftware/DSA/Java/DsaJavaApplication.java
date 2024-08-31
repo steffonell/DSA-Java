91d00634c87f0fd873443d0d2b1b0dfb5d3c9ea3
Stefan Dakic <Stefan.Dakic@stoecklin.com>
2024-08-31 15:19:05 +0200

    Project setup commit

---
+++ src/main/java/com/virtualarcsoftware/DSA/Java/DsaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.virtualarcsoftware.DSA.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaJavaApplication.class, args);
+	}
+
+}
